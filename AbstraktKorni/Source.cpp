7928baf7d9fa55149d7a99145c305f5d84bcf98f
My Name <kiykobogdan@gmail.com>
2022-10-27 18:04:00 +0300

    git

--- AbstraktKorni/Source.cpp
+++ AbstraktKorni/Source.cpp
@@ -5,7 +5,7 @@ using namespace std;
 class Korni
 {
 public:
-    virtual std::vector<double> rb(void) const = 0;
+    virtual vector<double> rb(void) const = 0;
 };
 
 class l1 : public Korni
@@ -16,7 +16,7 @@ public:
     void inb(double _b) { b = _b; }
     l1(void) : k(0), b(0) { }
     l1(double _k, double _b) : k(_k), b(_b) { }
-    virtual std::vector<double> rb(void) const override { return (fabs(b) < 1e-6 ? std::vector<double>() : std::vector<double>() = { -b / k }); }
+    virtual vector<double> rb(void) const override { return (fabs(b) < 1e-6 ?  vector<double>() :  vector<double>() = { -b / k }); }
 };
 
 class Yravnni : public Korni
@@ -28,10 +28,10 @@ public:
     void setC(double _c) { c = _c; }
     Yravnni(void) : a(0), b(0), c(0) { }
     Yravnni(double _a, double _b, double _c) : a(_a), b(_b), c(_c) { }
-    virtual std::vector<double> rb(void) const override
+    virtual vector<double> rb(void) const override
     {
         double D = b * b - 4 * a * c;
-        return (D < 0 ? std::vector<double>() : fabs(a) < 1e-6 ? l1(b, c).rb() : std::vector<double>() = { (-b + sqrt(D)) / (2 * a), (-b - sqrt(D)) / (2 * a) });
+        return (D < 0 ?  vector<double>() : fabs(a) < 1e-6 ? l1(b, c).rb() :  vector<double>() = { (-b + sqrt(D)) / (2 * a), (-b - sqrt(D)) / (2 * a) });
     }
 };
 
@@ -39,6 +39,6 @@ int main()
 {
     Yravnni obj(2, -12, 6);
     for (auto a : obj.rb())
-        std::cout << a << std::endl;
+       cout << a << std::endl;
 
 }
